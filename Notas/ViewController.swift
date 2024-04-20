4564974d71d04b0c376fafc3f342c46d11ae3c75
Juan Carlos Díaz <pinfabu@gmail.com>
2024-04-19 19:19:20 -0600

    Initial Commit

---
+++ Notas/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Notas
+//
+//  Created by Juan Carlos Díaz Valenzuela on 19/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
