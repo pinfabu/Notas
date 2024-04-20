00848a5e37eb21ee0b6f7d311edb3e17725f4f88
Juan Carlos Díaz <pinfabu@gmail.com>
2024-04-19 20:43:14 -0600

    Initial version

---
+++ Notas/Model/Note.swift
@@ -0,0 +1,14 @@
+//
+//  File.swift
+//  Notas
+//
+//  Created by Juan Carlos Díaz Valenzuela on 19/04/24.
+//
+
+import Foundation
+
+struct Note : Codable {
+    var title : String
+    var content : String
+    var date : Date
+}
---
+++ Notas/Model/NotesManager.swift
@@ -0,0 +1,8 @@
+//
+//  NotesManager.swift
+//  Notas
+//
+//  Created by Juan Carlos Díaz Valenzuela on 19/04/24.
+//
+
+import Foundation
